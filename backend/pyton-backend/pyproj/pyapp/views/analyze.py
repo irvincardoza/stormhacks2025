bf0dd69387fceb8f3ce787a0fb96a779b417918a
irvin <inc3@sfu.ca>
2025-10-04 18:55:40 -0700

    analzye

--- backend/pyton-backend/pyproj/pyapp/views/analyze.py
+++ backend/pyton-backend/pyproj/pyapp/views/analyze.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
+import sys, subprocess
+from pathlib import Path
 
 from ..utils.gemini_api import analyze_image
 from ..services.paths import BASE_DIR, SCREENSHOT_DIR, HISTORY_FILE
@@ -60,6 +62,8 @@ def analyze_screenshot(request):
 
     # Append ONLY; no reads
     append_history(entry)
+    subprocess.Popen([sys.executable, str((BASE_DIR / "pyapp" / "utils" / "script_combiner.py").resolve())])
+
 
     # Return a simple acknowledgment
     return JsonResponse({
